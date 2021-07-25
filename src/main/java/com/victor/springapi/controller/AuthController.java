934070c0261b23671b6791efb0643e26a0814c50
victor santos <victor.santos@neomind.com.br>
2021-07-25 18:42:25 -0300

    77. Refresh token

---
+++ src/main/java/com/victor/springapi/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.victor.springapi.controller;
+
+import com.victor.springapi.security.JWTUtil;
+import com.victor.springapi.security.UserSS;
+import com.victor.springapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value="/refresh_token", method= RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
